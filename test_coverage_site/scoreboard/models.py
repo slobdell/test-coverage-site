75a704ca07c174fc26b6745a7eacd273c8b404b3
Scott Lobdell <slobdell@hearsaycorp.com>
2015-05-04 09:50:14 -0700

    updates

--- test_coverage_site/scoreboard/models.py
+++ test_coverage_site/scoreboard/models.py
@@ -1,8 +1,12 @@
 import datetime
 import json
+import sys
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+
+DEFAULT_MIN_AUTHORS = 5
+DEFAULT_DISPLAY_PERCENT = 0.3
 
 
 class _ScoreBoard(models.Model):
@@ -36,7 +40,7 @@ class ScoreBoard(object):
     @classmethod
     def get_latest(cls, project_name):
         try:
-            _scoreboard = _ScoreBoard.objects.filter(project_name="test_run").latest("date")
+            _scoreboard = _ScoreBoard.objects.filter(project_name=project_name).latest("date")
         except ObjectDoesNotExist:
             pass  # SBL FIXME USE NULL OBJECT PATTERN
         return cls(_scoreboard)
@@ -45,7 +49,7 @@ class ScoreBoard(object):
     def get_on_date(cls, project_name, date):
         try:
             _scoreboard = _ScoreBoard.objects.filter(
-                project_name="test_run",
+                project_name=project_name,
                 date__lte=date
             ).latest("date")
         except ObjectDoesNotExist:
@@ -64,5 +68,26 @@ class ScoreBoard(object):
             author, score = ranked_rows[index]
             score *= 100.0
             line_count = json_dict["line_counts"][author]
-            ranked_rows[index] = (author, score, line_count,)
+            display_line_count = self._format_line_count(line_count)
+            ranked_rows[index] = (author, score, display_line_count)
+
+        ranked_rows = self._truncate_rows(ranked_rows)
         return ranked_rows
+
+    def _format_line_count(self, line_count):
+        thresh_to_str = {
+            500: "Less Than 500 lines",
+            10000: "500 to 10,000 lines",
+            sys.maxint: "Greater than 10,000 lines",
+        }
+        for thresh, display_str in sorted(thresh_to_str.items(), key=lambda t: t[0]):
+            if line_count < thresh:
+                return display_str
+
+    def _truncate_rows(self, ranked_rows):
+        json_dict = json.loads(self._scoreboard.json_data)  # TODO: cache that value
+
+        min_authors = json_dict.get("min_authors", DEFAULT_MIN_AUTHORS)
+        display_percent = json_dict.get("display_percent", DEFAULT_DISPLAY_PERCENT)
+        cutoff_count = max(int(len(ranked_rows) * display_percent), min_authors)
+        return ranked_rows[:cutoff_count]
